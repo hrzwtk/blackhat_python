6bf010755bf629b453be9f3320a25cc6863d8acd
hrzwtk <tuckey.3@gmail.com>
2022-04-20 17:59:45 +0900

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Black Hat Python
+
+% python3 -V  
+Python 3.8.9  
+  
+% python3 -m venv .venv  
+  
+% source .venv/bin/activate  
--- chapter-02/netcat.py
+++ chapter-02/netcat.py
@@ -9,6 +9,7 @@ import textwrap
 import threading
 
 def execute(cmd):
+    # cmdの両端の空白を削除
     cmd = cmd.strip()
     if not cmd:
         return
@@ -19,7 +20,7 @@ def execute(cmd):
     else:
         shell = False
     
-    # ローカルのOS上でコマンド実行
+    # ローカルのOS上でコマンド実行し、結果を取得
     output = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT, shell=shell)
 
     if locale.getdefaultlocale() == ('ja_JP', 'cp932'):
@@ -42,12 +43,25 @@ class NetCat:
         else:
             self.send()
 
+    def listen(self):
+        self.socket.bind((self.args.target, self.args.port))
+        self.socket.listen(5)
+        while True:
+            client_socket, _ = self.socket.accept()
+            # 戻り値は(conn object, address)
+            client_thread = threading.Thread(
+                target=self.handle, args=(client_socket,)
+                # argsはタプルなので、「,」が必要
+            )
+            client_thread.start()
+
     def send(self):
         self.socket.connect((self.args.target, self.args.port))
         if self.buffer:
             self.socket.send(self.buffer)
         
         try:
+            # targetからのレスポンスに対する処理を継続する
             while True:
                 recv_len = 1
                 response = ''
@@ -59,50 +73,48 @@ class NetCat:
                         break
                 if response:
                     print(response)
+                    # レスポンスを表示して、リクエストする文字列を受け付ける
                     buffer = input('> ')
                     buffer += '\n'
                     self.socket.send(buffer.encode())
         except KeyboardInterrupt:
+            # CTRL+Cでソケットを停止
             print('User terminated.')
             self.socket.close()
             sys.exit()
-        except EOFError:
+        except EOFError as e:
             print(e)
     
-    def listen(self):
-        self.socket.bind((self.args.target, self.args.port))
-        self.socket.listen(5)
-        while True:
-            client_socket, _ = self.socket.accept()
-            client_thread = threading.Thread(
-                target=self.handle, args=(client_socket,)
-            )
-            client_thread.start()
-    
     def handle(self, client_socket):
+        # コマンド実行。execute関数での実行結果をソケットに返す
         if self.args.execute:
             output = execute(self.args.execute)
             client_socket.send(output.encode())
+        # ファイルアップロード
         elif self.args.upload:
             file_buffer = b''
+            # ソケットでデータ受信を継続
             while True:
                 data = client_socket.recv(4096)
                 if data:
                     file_buffer += data
                 else:
                     break
-            
+            # 受信したデータをファイルに書き込む
             with open(self.args.upload, 'wb') as f:
                 f.write(file_buffer)
             message = f'Saved file {self.args.upload}'
             client_socket.send(message.encode())
+        # シェル起動
         elif self.args.command:
             cmd_buffer = b''
             while True:
                 try:
                     client_socket.send(b'<BHP:#> ')
+                    # Enter入力までソケットからデータを読み込む
                     while '\n' not in cmd_buffer.decode():
                         cmd_buffer += client_socket.recv(64)
+                    # execute関数での実行結果をソケットに返す
                     response = execute(cmd_buffer.decode())
                     if response:
                         client_socket.send(response.encode())
